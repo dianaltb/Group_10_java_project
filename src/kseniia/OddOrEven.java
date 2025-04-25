559f608375bf972adce28f7378883968b95bc460
kseni <ksenia.makhmudova@gmail.com>
2025-04-25 14:12:00 -0400

    Java question 01 - Odd or Even

---
+++ src/kseniia/OddOrEven.java
@@ -0,0 +1,20 @@
+package kseniia;
+
+public class OddOrEven {
+
+    public static void main(String[] args) {
+
+        int num = 6;
+        String result = "";
+
+
+        if(num %2 == 0){
+            result = "Number " + num + " is even";
+        }else{
+            result = "Number " + num + " is odd";
+        }
+        System.out.println(result);
+    }
+}
+
+
