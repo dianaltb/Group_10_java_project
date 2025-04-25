bdcaaa740e2705742ea84c477ec86c0f68f10855
kseni <ksenia.makhmudova@gmail.com>
2025-04-25 16:47:24 -0400

    Java question 01 - Odd or Even

--- src/kseniia/OddOrEven.java
+++ src/kseniia/OddOrEven.java
@@ -5,15 +5,15 @@ public class OddOrEven {
     public static void main(String[] args) {
 
         int num = 5;
-        String result = "";
+
 
 
         if(num % 2 == 0){
-            result = "Number " + num + " is even";
+            System.out.println("Number " + num + " is even");
         }else if(num % 2 != 0){
-            result = "Number " + num + " is odd";
+            System.out.println("Number " + num + " is odd");
         }
-        System.out.println(result);
+
     }
 }
 
