b0267a9b9fe1bf1f40b360d365ebf947763037c5
Niloofar1365 <Lotus1365@yahoo.com>
2025-04-25 19:53:53 -0400

    Niloo_Edited_identify_OddOrEven_task

--- src/niloo/OddOrEven.java
+++ src/niloo/OddOrEven.java
@@ -1,24 +1,40 @@
 package niloo;
 
-/*  Write  a method which can identify given number is even or odd
+/* Task: Write  a method which can identify given number is even or odd
   Output ex:
   identify(5); ->"Odd"
   identify(6); ->"Even"
    */
 public class OddOrEven {
 
-    public static String identify(int num) {
+    // creating identifyNum() method with String return type:
+    public static String identifyNum(int num) {
+
         if (num % 2 == 1) {
+            System.out.println("\"" + "Odd" + "\"");
             return "Odd";
         } else {
+            System.out.println("\"" + "Even" + "\"");
             return "Even";
+        }
+    }
+    // creating identify() method with void return type:
+    public static void identify(int num) {
+
+        if (num % 2 == 1) {
+            System.out.println("\"" + "Odd" + "\"");
+        } else {
+            System.out.println("\"" + "Even" + "\"");
         }
     }
 
     public static void main(String[] args) {
 
-        System.out.println("\"" + identify(5) + "\"");
-        System.out.println("\"" + identify(6) + "\"");
+        identify(5);
+        identify(6);
+
+        identifyNum(5);
+        identifyNum(6);
     }
 }
 
