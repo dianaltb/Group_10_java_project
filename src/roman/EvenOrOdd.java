ae766c55096888ec54782542ca98cdcb0a1def4d
roma3 <roma38lorde@gmail.com>
2025-04-26 20:10:53 -0400

    Created roman branch from master, on my local repository. Added package name roman, and created class name EvenOrOdd with method inside of that class called evenOrOdd, with some description what that method do.

---
+++ src/roman/EvenOrOdd.java
@@ -0,0 +1,15 @@
+package roman;
+
+public class EvenOrOdd {
+    //Method below is static method that can be called by class name an accept int parameter, and will print you statement if number is even or odd, if number is less than 1 it will print you err msg and will exit program.
+    public static void evenOrOdd(int num){
+        if (num < 1){
+            System.err.println("Number can't be less than 1");
+            System.exit(1);
+        }else if (num % 2 == 0){
+            System.out.println(num + " is Even");
+        }else {
+            System.out.println(num + " is Odd");
+        }
+    }
+}
