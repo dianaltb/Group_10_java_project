5b43cec0c8fb61310be76f3bbc669d3990b772ac
Elena Zenchenko <olenazenchenko@gmail.com>
2025-04-27 14:11:40 -0400

    odd or even number task

---
+++ src/olena/OddOrEven.java
@@ -0,0 +1,21 @@
+package olena;
+
+public class OddOrEven {
+
+            public static void main(String[] args) {
+                identify(6);
+                identify(5);
+
+
+            }
+
+            public static void identify(int num) {
+
+                if (num % 2 == 0) {
+                    System.out.println(num + " is the even number");
+                } else {
+                    System.out.println(num + " is an odd number");
+                }
+            }
+        }
+
