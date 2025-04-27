600ede9fd25a16d967b29809ab754150ddd09f36
roma3 <roma38lorde@gmail.com>
2025-04-27 09:45:17 -0400

    Updated name method name, make some changes in method

--- src/roman/EvenOrOdd.java
+++ src/roman/EvenOrOdd.java
@@ -2,11 +2,8 @@ package roman;
 
 public class EvenOrOdd {
     //Method below is static method that can be called by class name an accept int parameter, and will print you statement if number is even or odd, if number is less than 1 it will print you err msg and will exit program.
-    public static void evenOrOdd(int num){
-        if (num < 1){
-            System.err.println("Number can't be less than 1");
-            System.exit(1);
-        }else if (num % 2 == 0){
+    public static void identify(int num){
+         if (num % 2 == 0){
             System.out.println(num + " is Even");
         }else {
             System.out.println(num + " is Odd");
@@ -15,7 +12,7 @@ public class EvenOrOdd {
 
     public static void main(String[] args) {
 
-        evenOrOdd(0);
+        identify(0);
 
     }
 }
---
+++ src/roman/TestClass.java
@@ -0,0 +1,9 @@
+package roman;
+
+public class TestClass {
+    public static void main(String[] args) {
+        //Test your code here
+
+        EvenOrOdd.identify(-4);
+    }
+}
