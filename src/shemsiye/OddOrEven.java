2ece4670a02f195bb1c45222b67a692554a59144
shemsiye <xemsiye05@gmail.com>
2025-04-28 15:59:18 -0400

    Odd or even commited

---
+++ src/shemsiye/OddOrEven.java
@@ -0,0 +1,18 @@
+package shemsiye;
+
+public class OddOrEven {
+    public static void main(String[] args) {
+        identify(6);
+        identify(5);
+    }
+
+    public static void identify(int num) {
+
+        if (num % 2 == 0) {
+            System.out.println(num + " is the even number");
+        } else {
+            System.out.println(num + " is an odd number");
+        }
+    }
+}
+
