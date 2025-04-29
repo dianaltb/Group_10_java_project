71e82f9b20f2a05e6e51f680f507135a5a58db25
emanuele_sulander <eesulander@yahoo.com>
2025-04-28 21:48:19 -0400

    OddOrEven Task - completed.

--- src/emanuele/Test.java
+++ src/emanuele/Test.java
@@ -1,8 +1,25 @@
 package emanuele;
 
 public class Test {
-
+    /* /*
+Task:Write  a method which can identify given number is even or odd
+         Output ex:
+         identify(5); ->"Odd"
+         identify(6); ->"Even"
+ */
     public static void main(String[] args) {
-        System.out.println("Hi");
+            System.out.println(identify(6));
+
+        }
+
+        public static String identify(int x){
+            String result;
+            if (x % 2 == 0) {
+                result = "Even";
+            } else {
+                result = "Odd";
+            }
+            return result;
+        }
     }
-}
+
