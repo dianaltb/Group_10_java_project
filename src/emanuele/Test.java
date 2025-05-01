ba97811fb3b27c0c6127094333dd63391d852b0c
emanuele_sulander <eesulander@yahoo.com>
2025-05-01 14:19:49 -0400

    OddOrEven Task - completed.

--- src/emanuele/Test.java
+++ src/emanuele/Test.java
@@ -8,11 +8,11 @@ Task:Write  a method which can identify given number is even or odd
          identify(6); ->"Even"
  */
     public static void main(String[] args) {
-            System.out.println(identify(6));
-
+            System.out.println(identifyOddOrEven(6));
+            System.out.println(identifyOddOrEven(5));
         }
 
-        public static String identify(int x){
+        public static String identifyOddOrEven(int x){
             String result;
             if (x % 2 == 0) {
                 result = "Even";
