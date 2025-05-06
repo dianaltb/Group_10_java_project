aa27dade2cc36521c43c89483e410c24a6635676
shaistar88 <Resepct@000>
2025-05-06 01:19:34 -0500

    FINRA  task completed.

--- src/shaista/FINRA.java
+++ src/shaista/FINRA.java
@@ -4,20 +4,24 @@ public class FINRA {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 30 ; i++) {
-            if(i % 3 == 0 && i % 5 == 0) {
+        printFINRA(1,30);
+
+    }
+
+    public static void printFINRA(int start, int end){
+        for (int i = start; i <= end ; i++) {
+            if(i % 3 == 0 && i % 5 == 0){
                 System.out.print("FINRA");
-            } else if (i % 3 == 0) {
+            }else if(i % 3 ==0){
                 System.out.print("FIN");
-            }else if (i % 5 == 0) {
+            }else if(i % 5 == 0){
                 System.out.print("RA");
             }else{
                 System.out.print(i + " ");
             }
-
         }
-
     }
+
 
 
 }
