876403a5c6f68c35996dc9cdca1d72d95c84ba8e
mahammadmamishzada <Mahammad.Mamishzada@gmail.com>
2025-05-07 17:35:45 +0400

    OddOrEven task

---
+++ src/maga/OddOrEven.java
@@ -0,0 +1,16 @@
+package maga;
+
+public class OddOrEven {
+    public static void main(String[] args) {
+        identify(5);
+        identify(7);
+    }
+    public static void identify(int num){
+        if (num%2==0){
+            System.out.println(num+ " is even number");
+        }else{
+            System.out.println(num+" is odd number");
+        }
+    }
+}
+
