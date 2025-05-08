05f52ef7d9affb9fd41b05f1fb0fe38d5e628552
mahammadmamishzada <Mahammad.Mamishzada@gmail.com>
2025-05-08 19:17:25 +0400

    OddOrEven task

---
+++ src/maga/FinRa.java
@@ -0,0 +1,48 @@
+package maga;
+
+public class FinRa {
+    public static void main(String[] args) {
+        finra(25);
+        System.out.println("=======================");
+        finra2(30);
+    }
+
+    public static void finra (int n){
+        for (int i = 1; i < n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FinRa");
+            } else if (i % 3 == 0) {
+                System.out.println("Fin");
+            } else if (i % 5 == 0) {
+                System.out.println("Ra");
+            } else {
+                System.out.println(" " + i);
+            }
+        }
+    }
+
+    public static void finra2 (int n){
+        for (int i = 0; i < n; i++) {
+            String str = "";
+
+            if(i % 3 == 0){
+                str += "Fin";
+            }
+
+            if(i % 5 == 0){
+                str += "Ra";
+            }
+
+            if(str.isEmpty()){
+                System.out.println(i);
+            }else {
+                System.out.println(str);
+            }
+
+        }
+
+
+    }
+
+
+}
