0d89cd306942960bd7a02acf3711ed2480cf6d91
sashk <sashkovnyc@gmail.com>
2025-05-21 17:35:07 -0400

    Java Question 05 - Frequency of Characters is done.

---
+++ src/egor/FreqOfChars.java
@@ -0,0 +1,27 @@
+package egor;
+
+import java.util.*;
+
+public class FreqOfChars {
+    public static void main(String[] args) {
+
+        System.out.println("Frequency of AAABBCDD is: " + freqOfChars("AAABBCDD"));
+        //Frequency of AAABBCDD is: A3B2C1D2
+
+    }
+
+    private static String freqOfChars(String str){
+        StringBuilder builder = new StringBuilder();
+        Map<Character, Integer> map = new HashMap<>();
+        List<Character> list= str.chars()
+                .mapToObj(c -> (char) c)
+                .toList();
+        for(char ch : list){
+            map.putIfAbsent(ch, Collections.frequency(list, ch));
+        }
+        for(char key : map.keySet()){
+            builder.append(key).append(map.get(key));
+        }
+        return builder.toString();
+    }
+}
--- src/niloo/Test.java
+++
@@ -1,7 +0,0 @@
-package niloo;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hello team!");
-    }
-}
