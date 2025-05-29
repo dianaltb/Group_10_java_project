7dadbb42cf98c90e2f7d399ba958c4256acd27d3
Elena Zenchenko <olenazenchenko@gmail.com>
2025-05-28 22:30:15 -0400

    remove duplicates task

---
+++ src/olena/RemoveDuplicates.java
@@ -0,0 +1,29 @@
+package olena;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+        String str = "AAABBBCCC";
+        System.out.println(removeDuplicates(str));
+    }
+
+    public static String removeDuplicates(String str) {
+
+        List<Character> vedro = new ArrayList<>();
+        for (char letter : str.toCharArray()){
+            if(!vedro.contains(letter)){
+            vedro.add(letter);
+        }
+        }
+
+        StringBuilder builder = new StringBuilder();
+        for (char letter : vedro){
+            builder.append(letter);
+        }
+        return  builder.toString();
+
+    }
+}
