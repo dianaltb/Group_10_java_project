3bb5c7cead93cdbab7ee3b61d85c09a2445142b5
rasanbg <153374653+rasanbg@users.noreply.github.com>
2025-07-09 17:53:56 +0200

    Done task minNumInArray

---
+++ src/rasha/minNumInArray.java
@@ -0,0 +1,27 @@
+package rasha;
+
+public class minNumInArray {
+
+    //Write the method that can find min number inside of array
+
+
+    public static void main(String[] args) {
+
+        int[] nums = {81, 49, 6, 7, 80, 9, 10};
+
+        System.out.println(minNum(nums));
+    }
+
+    public static int minNum(int[] nums) {
+
+        int min = nums[0];
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (min > nums[i]) {
+                min = nums[i];
+            }
+        }
+        return min;
+    }
+}
